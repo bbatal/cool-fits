c4632fb7c1edc38a0b8d45fd105d635bc7f5a942
bart batalinski <bartbatalinski@gmail.com>
2023-12-08 10:52:33 -0500

    (feature): added header, navigation and global page component to appear on evvery page. Integrated server side stylesheet rendering via _document file and added nprogress bar by tapping into next/router events

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,9 +4,9 @@ export default function Nav() {
   return (
     <nav>
       <Link href="/products">Products</Link>
-      <Link href="sell">Sell</Link>
-      <Link href="orders">Orders</Link>
-      <Link href="account">Account</Link>
+      <Link href="/sell">Sell</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
     </nav>
   );
 }
